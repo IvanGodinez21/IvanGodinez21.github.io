ef6dda9272a53f1f7a4e4ab3e6f2b29c8c7be94d
Jesús Iván Godínez Martínez <ivano.godinez@gmail.com>
2023-07-15 14:27:34 -0600

    feat: :sparkles: add `CardsDiscordActivity` component
    add:
    - types inside file

---
+++ src/components/cards/discord_activity.vue
@@ -0,0 +1,47 @@
+<script lang="ts" setup>
+import Lanyard from '@/classes/lanyard';
+import { Activities, LanyardSocketEvents } from '@/constants/lanyard';
+import { ILanyardResponse } from '@/types/lanyard';
+import { IUser } from '@/types/user';
+
+const props = defineProps<{
+  user: IUser;
+}>();
+
+const discord = ref<IUser['social']['discord']>();
+
+const mainActivity = computed(() =>
+  discord.value?.activities?.findLast((activity) => activity.type !== Activities.Custom)
+);
+
+onMounted(() => {
+  function loadUser() {
+    if (!props.user.social.discord.id ?? !props.user.social.discord.discord_user?.id) return;
+    const lanyard = new Lanyard({
+      userId: props.user.social.discord.id ?? props.user.social.discord.discord_user?.id,
+    });
+    function handler(data: ILanyardResponse) {
+      discord.value = data;
+    }
+    lanyard.connect();
+    lanyard.on(LanyardSocketEvents.initState, handler);
+    lanyard.on(LanyardSocketEvents.presenceUpdate, handler);
+    onUnmounted(() => {
+      lanyard.off(LanyardSocketEvents.initState, handler);
+      lanyard.off(LanyardSocketEvents.presenceUpdate, handler);
+      lanyard.disconnect();
+    });
+  }
+  loadUser();
+});
+</script>
+<template>
+  <div
+    v-if="mainActivity"
+    class="bg-medium-spring-green dark:bg-prussian-blue p-3 rounded-lg flex flex-col justify-center items-center"
+  >
+    <p class="text-center">
+      <span class="font-semibold">{{ Activities[mainActivity.type] }}</span> {{ mainActivity.name }}
+    </p>
+  </div>
+</template>
